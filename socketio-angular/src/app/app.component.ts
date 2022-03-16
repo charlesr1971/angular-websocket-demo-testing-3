d843aaf1605742ca32f6f26933495602a3c3468a
Charles Robertson <cdesign@btinternet.com>
2022-03-16 22:19:50 +0000

    commit

--- socketio-angular/src/app/app.component.ts
+++ socketio-angular/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { SocketioService } from './socketio.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
 import { UtilsService } from './core/services/utils/utils.service';
 
 // static data only for demo purposes, in real world scenario, this would be already stored on client
 const SENDER = {
-  id: "123",
-  name: "John Doe",
+  id: '123',
+  name: 'John Doe',
 };
 @Component({
   selector: 'app-root',
@@ -16,12 +16,16 @@ const SENDER = {
 })
 
 export class AppComponent implements OnInit {
+
+  @ViewChild('messagesDiv') messagesDiv: ElementRef;
+
   title = 'socketio-angular';
   heading = 'Web socket with Camunda Demo';
 
-  CHAT_ROOM = "myRandomChatRoomId";
+  CHAT_ROOM = 'myRandomChatRoomId';
 
-  token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.DHRRG7N3wUVftglHa5cD231vRrdD2Tn_NPUVoxoY1Ng';
+  token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwMDAwMDAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.q02s3D_xHjIQG5dwuDyxPpMTp_Hby6viFspsA_cwp4A';
+  token2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwMDAwMDEiLCJuYW1lIjoiSmFuZSBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.X2iVjosWc8az0gAEaqCUh1lU3ZPF2VZf1vLowy-2z-g';
 
   messages = [];
 
@@ -36,6 +40,7 @@ export class AppComponent implements OnInit {
   constructor(
     private socketService: SocketioService,
     private formBuilder: FormBuilder,
+    private renderer:Renderer2,
     private utilsService: UtilsService
   ) {}
 
@@ -48,8 +53,10 @@ export class AppComponent implements OnInit {
     if (token) {
       this.socketService.setupSocketConnection(token);
       this.socketService.subscribeToMessages((err, data) => {
-        console.log("NEW MESSAGE ", data);
+        console.log('NEW MESSAGE ', data);
         this.messages = [...this.messages, data];
+        this.messages = this.messages.filter((v, i, a) => a.findIndex(t => (t.id === v.id && t.message.toLowerCase() === v.message.toLowerCase())) === i);
+        console.log('AppComponent: submitToken(): this.messages: ', this.messages);
       });
     }
   }
@@ -57,19 +64,25 @@ export class AppComponent implements OnInit {
   submitMessage() {
     const message = this.messageForm.get('message').value;
     if (message) {
+      //this.clearDiv();
       this.socketService.sendMessage({message, roomName: this.CHAT_ROOM}, cb => {
-        console.log("ACKNOWLEDGEMENT ", cb);
+        console.log('ACKNOWLEDGEMENT ', cb);
       });
-      this.messages = [
+      /* this.messages = [
         ...this.messages,
         {
           message,
           ...SENDER,
         },
       ];
+      console.log('AppComponent: submitMessage(): this.messages: ', this.messages); */
       // clear the input after the message is sent
       this.messageForm.reset();
     }
+  }
+
+  clearDiv() {
+    this.renderer.setProperty(this.messagesDiv.nativeElement, 'innerHTML', '')
   }
 
   ngOnDestroy() {
